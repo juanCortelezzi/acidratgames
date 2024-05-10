09f87ff0c7360a0dba0b35a27266f2a40382339f
J.B.C <juanbicortelezzi@gmail.com>
2024-05-10 22:47:53 +1200

    better stuff

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,7 +3,6 @@ const config = {
   trailingComma: "all",
   printWidth: 80,
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
-  pluginSearchDirs: false,
 };
 
 export default config;
